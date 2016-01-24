af51eb501e80baf9619a98d65de26f10ff0a4aa5
niptim <s.moliner.reig@gmail.com>
2016-01-24 19:50:31 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -5,6 +5,7 @@ echo $hora;
 if($payload->ref && $payload->ref=='refs/heads/master'){
 	$file = fopen("log.txt", "a");
 	fwrite($file, $hora . PHP_EOL);
+	fwrite($file, $_POST[0] . PHP_EOL);
 	fclose($file);
 	shell_exec('./PUESTA_EN_PRODUCCION.sh');
 }else{

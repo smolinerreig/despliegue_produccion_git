ae9ef7c54a1c6e818ca09fa0b85183f8f9105e82
niptim <s.moliner.reig@gmail.com>
2016-01-24 19:58:10 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -1,6 +1,6 @@
 <?php 
 $hora='Actualización del servidor a las '.getdate()['hours'].':'.getdate()['minutes'].':'.getdate()['seconds'];
-$payload = json_decode($_POST)['payload'];
+$payload = json_decode($_POST);
 echo $hora;
 
 if($payload->ref && $payload->ref=='refs/heads/master'){

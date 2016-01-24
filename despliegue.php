8b386b267e6915924e98d963c369d74e583c0e1a
niptim <s.moliner.reig@gmail.com>
2016-01-24 19:23:44 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -1,6 +1,6 @@
 <?php 
 $hora='Actualización del servidor a las '.getdate()['hours'].':'.getdate()['minutes'].':'.getdate()['seconds'];
-$payload = json_decode($POST['payload']);
+$payload = json_decode($_POST['payload']);
 echo $hora;
 echo $payload; 
 

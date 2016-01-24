123ba6b180b37f7c28f137aa78b350db91872648
niptim <s.moliner.reig@gmail.com>
2016-01-24 19:28:08 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -5,9 +5,9 @@ echo $hora;
 echo $payload; 
 $file = fopen("log.txt", "a");
 	fwrite($file, $hora . PHP_EOL);
-	fwrite($file, $payload->ref . PHP_EOL);
+	fwrite($file, $payload . PHP_EOL);
 	fclose($file);
-	
+
 /*if($payload->ref=='refs/heads/master'){
 	$file = fopen("log.txt", "a");
 	fwrite($file, $hora . PHP_EOL);

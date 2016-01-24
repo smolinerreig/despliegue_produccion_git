0fc8155a18862891fce362374ec52b664ecbd871
niptim <s.moliner.reig@gmail.com>
2016-01-24 12:11:55 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -4,7 +4,6 @@ echo $hora;
 if(json_decode($_POST['payload'])->ref=='refs/heads/master'){
 	if(!file_exists('log.txt')){
 		shell_exec('touch log.txt');
-
 		shell_exec('echo'.$hora.'  >> log.txt');
 	}else{
 		shell_exec('echo '.$hora.' >> log.txt');

5b09b9b51267df790ce850240da12bb8edf5b6cb
niptim <s.moliner.reig@gmail.com>
2016-01-24 13:37:39 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -5,12 +5,12 @@ echo $hora;
 if($payload->ref=='refs/heads/master'){
 	$file = fopen("log.txt", "a");
 	fwrite($file, $hora . PHP_EOL);
-	fwrite($file, echo $payload->ref . PHP_EOL);
+	fwrite($file, $payload->ref . PHP_EOL);
 
 	fclose($file);
 }else{
 	$file = fopen("log.txt", "a");
-	fwrite($file, json_decode($_POST)[0] . PHP_EOL);
+	fwrite($file, $payload->ref . PHP_EOL);
 	fclose($file);
 }
 	

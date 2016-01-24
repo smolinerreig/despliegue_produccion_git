da966e02a7a7e6042bf58a314a73984ab4df7bcf
niptim <s.moliner.reig@gmail.com>
2016-01-24 19:52:16 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -1,12 +1,12 @@
 <?php 
 $hora='Actualización del servidor a las '.getdate()['hours'].':'.getdate()['minutes'].':'.getdate()['seconds'];
-$payload = json_decode($_POST);
+$payload = json_decode($_POST[0]);
 echo $hora;
 
 if($payload->ref && $payload->ref=='refs/heads/master'){
 	$file = fopen("log.txt", "a");
 	fwrite($file, $hora . PHP_EOL);
-	fwrite($file, $_POST[0] . PHP_EOL);
+	fwrite($file, $payload . PHP_EOL);
 	fclose($file);
 	shell_exec('./PUESTA_EN_PRODUCCION.sh');
 }else{

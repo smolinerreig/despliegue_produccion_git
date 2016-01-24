dcf105a5d898f824d933df3d15e114ccd390c41b
niptim <s.moliner.reig@gmail.com>
2016-01-24 12:11:02 +0100

    más de lo mismo

--- despliegue.php
+++ despliegue.php
@@ -1,10 +1,10 @@
 <?php 
 $hora='Actualización del servidor a las '.getdate()['hours'].':'.getdate()['minutes'].':'.getdate()['seconds'];
 echo $hora;
-if(json_decode($payload)->ref=='refs/heads/master'){
+if(json_decode($_POST['payload'])->ref=='refs/heads/master'){
 	if(!file_exists('log.txt')){
 		shell_exec('touch log.txt');
-		
+
 		shell_exec('echo'.$hora.'  >> log.txt');
 	}else{
 		shell_exec('echo '.$hora.' >> log.txt');

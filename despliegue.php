98bcdf1ae4fe4906f795e8fe01e390a56bfcfb34
niptim <s.moliner.reig@gmail.com>
2016-01-24 11:09:40 +0100

    meh

--- despliegue.php
+++ despliegue.php
@@ -1,5 +1,168 @@
 <?php 
-$pay=json_decode($_POST['payload']);
-if($pay && $pay->ref=='master'){
+$pay=json_decode('{
+  "ref": "refs/heads/master",
+  "before": "646edbea887528f3765bf4d7d1e6c1d8c5ee15a1",
+  "after": "ec88a72c07c6a11647eaa6dbecfcfbba5de7c7c3",
+  "created": false,
+  "deleted": false,
+  "forced": false,
+  "base_ref": null,
+  "compare": "https://github.com/smolinerreig/despliegue_produccion_git/compare/646edbea8875...ec88a72c07c6",
+  "commits": [
+    {
+      "id": "ec88a72c07c6a11647eaa6dbecfcfbba5de7c7c3",
+      "distinct": true,
+      "message": "más de lo mismo",
+      "timestamp": "2016-01-24T10:35:58+01:00",
+      "url": "https://github.com/smolinerreig/despliegue_produccion_git/commit/ec88a72c07c6a11647eaa6dbecfcfbba5de7c7c3",
+      "author": {
+        "name": "niptim",
+        "email": "s.moliner.reig@gmail.com",
+        "username": "smolinerreig"
+      },
+      "committer": {
+        "name": "niptim",
+        "email": "s.moliner.reig@gmail.com",
+        "username": "smolinerreig"
+      },
+      "added": [
+
+      ],
+      "removed": [
+
+      ],
+      "modified": [
+        "despliegue.php",
+        "despliegue.php~"
+      ]
+    }
+  ],
+  "head_commit": {
+    "id": "ec88a72c07c6a11647eaa6dbecfcfbba5de7c7c3",
+    "distinct": true,
+    "message": "más de lo mismo",
+    "timestamp": "2016-01-24T10:35:58+01:00",
+    "url": "https://github.com/smolinerreig/despliegue_produccion_git/commit/ec88a72c07c6a11647eaa6dbecfcfbba5de7c7c3",
+    "author": {
+      "name": "niptim",
+      "email": "s.moliner.reig@gmail.com",
+      "username": "smolinerreig"
+    },
+    "committer": {
+      "name": "niptim",
+      "email": "s.moliner.reig@gmail.com",
+      "username": "smolinerreig"
+    },
+    "added": [
+
+    ],
+    "removed": [
+
+    ],
+    "modified": [
+      "despliegue.php",
+      "despliegue.php~"
+    ]
+  },
+  "repository": {
+    "id": 49447830,
+    "name": "despliegue_produccion_git",
+    "full_name": "smolinerreig/despliegue_produccion_git",
+    "owner": {
+      "name": "smolinerreig",
+      "email": "s.moliner.reig@gmail.com"
+    },
+    "private": false,
+    "html_url": "https://github.com/smolinerreig/despliegue_produccion_git",
+    "description": "",
+    "fork": false,
+    "url": "https://github.com/smolinerreig/despliegue_produccion_git",
+    "forks_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/forks",
+    "keys_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/keys{/key_id}",
+    "collaborators_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/collaborators{/collaborator}",
+    "teams_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/teams",
+    "hooks_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/hooks",
+    "issue_events_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/issues/events{/number}",
+    "events_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/events",
+    "assignees_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/assignees{/user}",
+    "branches_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/branches{/branch}",
+    "tags_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/tags",
+    "blobs_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/git/blobs{/sha}",
+    "git_tags_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/git/tags{/sha}",
+    "git_refs_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/git/refs{/sha}",
+    "trees_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/git/trees{/sha}",
+    "statuses_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/statuses/{sha}",
+    "languages_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/languages",
+    "stargazers_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/stargazers",
+    "contributors_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/contributors",
+    "subscribers_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/subscribers",
+    "subscription_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/subscription",
+    "commits_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/commits{/sha}",
+    "git_commits_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/git/commits{/sha}",
+    "comments_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/comments{/number}",
+    "issue_comment_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/issues/comments{/number}",
+    "contents_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/contents/{+path}",
+    "compare_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/compare/{base}...{head}",
+    "merges_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/merges",
+    "archive_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/{archive_format}{/ref}",
+    "downloads_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/downloads",
+    "issues_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/issues{/number}",
+    "pulls_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/pulls{/number}",
+    "milestones_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/milestones{/number}",
+    "notifications_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/notifications{?since,all,participating}",
+    "labels_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/labels{/name}",
+    "releases_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/releases{/id}",
+    "deployments_url": "https://api.github.com/repos/smolinerreig/despliegue_produccion_git/deployments",
+    "created_at": 1452539389,
+    "updated_at": "2016-01-11T19:13:05Z",
+    "pushed_at": 1453628175,
+    "git_url": "git://github.com/smolinerreig/despliegue_produccion_git.git",
+    "ssh_url": "git@github.com:smolinerreig/despliegue_produccion_git.git",
+    "clone_url": "https://github.com/smolinerreig/despliegue_produccion_git.git",
+    "svn_url": "https://github.com/smolinerreig/despliegue_produccion_git",
+    "homepage": null,
+    "size": 570,
+    "stargazers_count": 0,
+    "watchers_count": 0,
+    "language": "PHP",
+    "has_issues": true,
+    "has_downloads": true,
+    "has_wiki": true,
+    "has_pages": false,
+    "forks_count": 0,
+    "mirror_url": null,
+    "open_issues_count": 0,
+    "forks": 0,
+    "open_issues": 0,
+    "watchers": 0,
+    "default_branch": "master",
+    "stargazers": 0,
+    "master_branch": "master"
+  },
+  "pusher": {
+    "name": "smolinerreig",
+    "email": "s.moliner.reig@gmail.com"
+  },
+  "sender": {
+    "login": "smolinerreig",
+    "id": 10488504,
+    "avatar_url": "https://avatars.githubusercontent.com/u/10488504?v=3",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/smolinerreig",
+    "html_url": "https://github.com/smolinerreig",
+    "followers_url": "https://api.github.com/users/smolinerreig/followers",
+    "following_url": "https://api.github.com/users/smolinerreig/following{/other_user}",
+    "gists_url": "https://api.github.com/users/smolinerreig/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/smolinerreig/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/smolinerreig/subscriptions",
+    "organizations_url": "https://api.github.com/users/smolinerreig/orgs",
+    "repos_url": "https://api.github.com/users/smolinerreig/repos",
+    "events_url": "https://api.github.com/users/smolinerreig/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/smolinerreig/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+}');
+if($pay && $pay->ref=='refs/heads/master'){
 }
 ?>

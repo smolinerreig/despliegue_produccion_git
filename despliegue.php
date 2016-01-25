cbc301d97e151fce6dcfe439618d93e9815ae4f1
niptim <s.moliner.reig@gmail.com>
2016-01-25 20:15:34 +0100

    Última prueba desarrollo

--- despliegue.php
+++ despliegue.php
@@ -2,10 +2,7 @@
 $hora='Actualización exitosa del servidor a las '.getdate()['hours'].':'.getdate()['minutes'].':'.getdate()['seconds'];
 $error='Actualización fallida del servidor a las '.getdate()['hours'].':'.getdate()['minutes'].':'.getdate()['seconds'];
 $payload = json_decode($_POST['payload']);
-echo $hora;
-$file = fopen("log.txt", "a");
-	fwrite($file, count($payload) . PHP_EOL);
-	fclose($file);
+
 if($payload->ref && $payload->ref=='refs/heads/master'){
 	$file = fopen("log.txt", "a");
 	fwrite($file, $hora . PHP_EOL);
